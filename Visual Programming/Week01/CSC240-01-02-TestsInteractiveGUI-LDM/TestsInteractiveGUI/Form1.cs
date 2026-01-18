5878bc5ac241bffefd3be00523eb9d8e2f92c252
L Dale M <lmumfo3853@my.sullivan.edu>
2026-01-17 22:40:14 -0500

    Created CSC240
    Copied files into new repository.

---
+++ Visual Programming/Week01/CSC240-01-02-TestsInteractiveGUI-LDM/TestsInteractiveGUI/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSC240_01_02_TestsInteractiveGUI_LDM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double test1 = Convert.ToDouble(txtTest1.Text);
+                double test2 = Convert.ToDouble(txtTest2.Text);
+                double test3 = Convert.ToDouble(txtTest3.Text);
+                double test4 = Convert.ToDouble(txtTest4.Text);
+                double test5 = Convert.ToDouble(txtTest5.Text);
+
+                double average = (test1 + test2 + test3 + test4 + test5) / 5.0;
+
+                lblResult.Text = average.ToString("F2");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid numeric values for all test scores.", 
+                    "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", 
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Visual Programming/Week01/CSC240-01-03-PigLatinGUI-LDM/CSC240-01-03-PigLatinGUI-LDM/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC240_01_03_PigLatinGUI_LDM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string inputWord = txtInput.Text.Trim();
+
+            if (string.IsNullOrEmpty(inputWord))
+            {
+                MessageBox.Show("Please enter a word.", "Input Required", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtInput.Focus();
+                return;
+            }
+
+            string pigLatinWord = ConvertToPigLatin(inputWord);
+            lblOutput.Text = pigLatinWord;
+        }
+
+        private string ConvertToPigLatin(string word)
+        {
+            if (word.Length == 0)
+                return word;
+
+            // Get the first letter and the rest of the word
+            char firstLetter = word[0];
+            string restOfWord = word.Substring(1);
+
+            // Create pig Latin: remove first letter, add it and "ay" at the end
+            string pigLatin = restOfWord + firstLetter + "ay";
+
+            return pigLatin;
+        }
+    }
+}
