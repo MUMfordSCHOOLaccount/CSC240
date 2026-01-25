41160a5ed516c4c6dfa2495af119332b6a4c6ae5
L Dale M <lmumfo3853@my.sullivan.edu>
2026-01-25 05:15:30 -0500

    Step3 PhoneCall
    Complete Step 3.

--- Visual Programming/Week03/CSC240-03-02-PhoneCall-LDM/Program.cs
+++ Visual Programming/Week03/CSC240-03-02-PhoneCall-LDM/Program.cs
@@ -2,4 +2,11 @@
 
 class PhoneCall
 {
+    private static double CalcPhoneCallPrice(int minutes, double rate)
+    {
+        const double base_fee = 0.25;
+        double call_fee;
+        call_fee = base_fee + minutes * rate;
+        return call_fee;
+    }
 }
