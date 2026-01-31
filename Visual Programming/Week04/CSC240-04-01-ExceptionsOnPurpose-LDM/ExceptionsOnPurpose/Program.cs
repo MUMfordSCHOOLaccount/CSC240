a03fbc22cf5f711494e36ad557e14da606ac810d
L Dale M <lmumfo3853@my.sullivan.edu>
2026-01-31 05:43:42 -0500

    complete step 3 add exception handleing to exceptionsonpurpose

--- Visual Programming/Week04/CSC240-04-01-ExceptionsOnPurpose-LDM/ExceptionsOnPurpose/Program.cs
+++ Visual Programming/Week04/CSC240-04-01-ExceptionsOnPurpose-LDM/ExceptionsOnPurpose/Program.cs
@@ -9,9 +9,24 @@ class ExceptionsOnPurpose
         int result;
         int zero = 0;
 
-        Write("Enter an integer >> ");
-        answer = Convert.ToInt32(ReadLine());
-        result = answer / zero; // will throw DivideByZeroException when executed
-        WriteLine("The answer is " + answer);
+        try
+        {
+            Write("Enter an integer >> ");
+            answer = Convert.ToInt32(ReadLine());
+            result = answer / zero; // will throw DivideByZeroException when executed
+            WriteLine("The answer is " + answer);
+        }
+        catch (FormatException ex)
+        {
+            WriteLine("Input was not in a correct format: " + ex.Message);
+        }
+        catch (DivideByZeroException ex)
+        {
+            WriteLine("Attempted to divide by zero: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            WriteLine("An unexpected error occurred: " + ex.Message);
+        }
     }
 }
