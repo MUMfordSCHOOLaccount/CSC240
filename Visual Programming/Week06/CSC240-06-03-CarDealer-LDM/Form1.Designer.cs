91d75fd6ed684bce630f08ae9ee3770c8d20394a
L Dale M <lmumfo3853@my.sullivan.edu>
2026-02-14 17:38:54 -0500

    Add using statements to Designer files for complete namespace resolution

--- Visual Programming/Week06/CSC240-06-03-CarDealer-LDM/Form1.Designer.cs
+++ Visual Programming/Week06/CSC240-06-03-CarDealer-LDM/Form1.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace CSC240_06_03_CarDealer_LDM
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CSC240_06_03_CarDealer_LDM
 {
     partial class CarSelectionForm
     {
