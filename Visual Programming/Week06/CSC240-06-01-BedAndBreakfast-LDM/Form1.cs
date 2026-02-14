d8ceec04d1bb2e51bf428397e3d5e0bbf1a8cc82
L Dale M <lmumfo3853@my.sullivan.edu>
2026-02-14 15:19:27 -0500

    Bed and Breakfast

---
+++ Visual Programming/Week06/CSC240-06-01-BedAndBreakfast-LDM/Form1.cs
@@ -0,0 +1,10 @@
+namespace CSC240_06_01_BedAndBreakfast_LDM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
