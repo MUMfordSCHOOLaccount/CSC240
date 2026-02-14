6e6a4e951b3c3e0da82c72bf8ad2e5649a73370a
L Dale M <lmumfo3853@my.sullivan.edu>
2026-02-14 15:35:55 -0500

    Add Show Rates button and click handler

--- Visual Programming/Week06/CSC240-06-01-BedAndBreakfast-LDM/Form1.Designer.cs
+++ Visual Programming/Week06/CSC240-06-01-BedAndBreakfast-LDM/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private System.Windows.Forms.Label welcomeLabel;
         private System.Windows.Forms.Label rateLabel;
+        private System.Windows.Forms.Button showRatesButton;
 
         private void InitializeComponent()
         {
@@ -56,11 +57,23 @@
             this.rateLabel.TabIndex = 1;
             this.rateLabel.Text = "Check our rates";
             // 
+            // showRatesButton
+            // 
+            this.showRatesButton = new System.Windows.Forms.Button();
+            this.showRatesButton.Location = new System.Drawing.Point(320, 160);
+            this.showRatesButton.Name = "showRatesButton";
+            this.showRatesButton.Size = new System.Drawing.Size(120, 36);
+            this.showRatesButton.TabIndex = 2;
+            this.showRatesButton.Text = "Show Rates";
+            this.showRatesButton.UseVisualStyleBackColor = true;
+            this.showRatesButton.Click += new System.EventHandler(this.showRatesButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Yellow;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showRatesButton);
             this.Controls.Add(this.rateLabel);
             this.Controls.Add(this.welcomeLabel);
             this.Name = "BaileysForm";
--- Visual Programming/Week06/CSC240-06-01-BedAndBreakfast-LDM/Form1.cs
+++ Visual Programming/Week06/CSC240-06-01-BedAndBreakfast-LDM/Form1.cs
@@ -6,5 +6,11 @@ namespace CSC240_06_01_BedAndBreakfast_LDM
         {
             InitializeComponent();
         }
+
+        private void showRatesButton_Click(object sender, EventArgs e)
+        {
+            string message = "Standard Room: $75 per night\nDeluxe Room: $120 per night\nSuite: $200 per night";
+            MessageBox.Show(message, "Room Rates", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
