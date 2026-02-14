9c7e86905177d24960804e0ed9aeb195e39b723f
L Dale M <lmumfo3853@my.sullivan.edu>
2026-02-14 15:59:43 -0500

    Move FiveColors2 project to Week06 folder and add form files

---
+++ Visual Programming/Week06/CSC240-06-01-BedAndBreakfast-LDM/Visual Programming/Week06/CSC240-06-02-FiveColors2-LDM/Program.cs
@@ -0,0 +1,12 @@
+namespace CSC240_06_02_FiveColors2_LDM
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FiveColorsForm());
+        }
+    }
+}
